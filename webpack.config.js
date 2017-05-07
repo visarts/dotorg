23bce6bac5b87f2e8900c5b15d07d0fdc1c8c8c0
Brendan Best <bestb12@uwgb.edu>
2017-05-06 20:18:28 -0500

    add metadata for content

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const ROOT_PATH = path.resolve(__dirname);
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [
