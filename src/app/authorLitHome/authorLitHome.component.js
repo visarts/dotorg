1bfc44e73487417a9ad0674a45d02322e13f0640
Brendan Best <visarts@gmail.com>
2017-05-25 18:17:52 -0500

    start work on the reader modal

--- src/app/authorLitHome/authorLitHome.component.js
+++ src/app/authorLitHome/authorLitHome.component.js
@@ -10,7 +10,7 @@ export default class AuthorLitHome extends React.Component {
     this.state = {
       show: true,
       readingModeClass: readingMode ? readingMode : 'darkMode',
-      readingModeText: readingMode && readingMode === 'lightMode' ? 'Set Dark Mode' : 'Set Light Mode'
+      readingModeText: readingMode && readingMode === 'lightMode' ? 'Lights Off' : 'Lights On'
     };
     this.currentWorkKey = props.match.params.work;
     this.content = require(`Literature/${this.props.data.authorKey}/${this.currentWorkKey}.html`);
@@ -31,7 +31,7 @@ export default class AuthorLitHome extends React.Component {
   setReadingMode () {
     const currentMode = localStorage.getItem('readingMode');
     const flipMode = currentMode && currentMode === 'darkMode' ? 'lightMode' : currentMode && currentMode === 'lightMode' ? 'darkMode' : 'darkMode';
-    this.setState({readingModeClass: flipMode, readingModeText: flipMode === 'lightMode' ? 'Set Dark Mode' : 'Set Light Mode'});
+    this.setState({readingModeClass: flipMode, readingModeText: flipMode === 'lightMode' ? 'Lights Off' : 'Lights On'});
     localStorage.setItem('readingMode', flipMode);
   }
 
@@ -41,15 +41,19 @@ export default class AuthorLitHome extends React.Component {
         <Modal
           show={this.state.show}
           onHide={this.hideModal.bind(this)}
-          dialogClassName={`custom-modal ${this.state.readingModeClass}`}>
+          dialogClassName="custom-modal">
           <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title-lg">
+            <h1>Portitude Reader</h1>
+          </Modal.Header>
+          <Modal.Header className="modalSubNav">
+            <strong>More by {this.props.data.fname} {this.props.data.lname}</strong>
+            <button onClick={this.setReadingMode.bind(this)} className="readingModeButton">{this.state.readingModeText}</button>
+          </Modal.Header>
+          <Modal.Body className={this.state.readingModeClass}>
+            <Modal.Title>
               <h1>{this.currentWork.title}</h1>
               <h2>{this.props.data.fname} {this.props.data.lname}</h2>
-              <button onClick={this.setReadingMode.bind(this)}>{this.state.readingModeText}</button>
             </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
             <div className="content" dangerouslySetInnerHTML={this.htmlContent}></div>
           </Modal.Body>
           <Modal.Footer>
