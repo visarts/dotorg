affe9ef6a71ad8da69d4f15b17f52621e3f8aa10
Brendan Best <visarts@gmail.com>
2017-05-24 22:01:18 -0500

    add light vs dark mode for reading

--- src/app/authorLitHome/authorLitHome.component.js
+++ src/app/authorLitHome/authorLitHome.component.js
@@ -6,13 +6,17 @@ export default class AuthorLitHome extends React.Component {
 
   constructor (props) {
     super(props);
+    const readingMode = localStorage.getItem('readingMode');
     this.state = {
-      show: true
+      show: true,
+      readingModeClass: readingMode ? readingMode : 'darkMode',
+      readingModeText: readingMode && readingMode === 'lightMode' ? 'Set Dark Mode' : 'Set Light Mode'
     };
     this.currentWorkKey = props.match.params.work;
     this.content = require(`Literature/${this.props.data.authorKey}/${this.currentWorkKey}.html`);
     this.currentWork = this.props.data.content.filter(item => item.fileName === this.currentWorkKey)[0];
     this.htmlContent = {__html: this.content};
+
   }
 
   showModal () {
@@ -24,17 +28,25 @@ export default class AuthorLitHome extends React.Component {
     location.hash = `#/literature/${this.props.data.authorKey}`;
   }
 
+  setReadingMode () {
+    const currentMode = localStorage.getItem('readingMode');
+    const flipMode = currentMode && currentMode === 'darkMode' ? 'lightMode' : currentMode && currentMode === 'lightMode' ? 'darkMode' : 'darkMode';
+    this.setState({readingModeClass: flipMode, readingModeText: flipMode === 'lightMode' ? 'Set Dark Mode' : 'Set Light Mode'});
+    localStorage.setItem('readingMode', flipMode);
+  }
+
   render () {
     return (
       <div className="authorLitHome">
         <Modal
           show={this.state.show}
           onHide={this.hideModal.bind(this)}
-          dialogClassName="custom-modal">
+          dialogClassName={`custom-modal ${this.state.readingModeClass}`}>
           <Modal.Header closeButton>
             <Modal.Title id="contained-modal-title-lg">
               <h1>{this.currentWork.title}</h1>
               <h2>{this.props.data.fname} {this.props.data.lname}</h2>
+              <button onClick={this.setReadingMode.bind(this)}>{this.state.readingModeText}</button>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
