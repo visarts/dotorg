b2903d2e08e82c9c72ba99ab7cffe793319e33c1
Brendan Best <visarts@gmail.com>
2017-08-13 13:51:04 -0500

    setup switching on global view routing

--- src/app/globalView/globalView.component.js
+++ src/app/globalView/globalView.component.js
@@ -1,51 +1,52 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import HomeView from './homeView/homeView.component';
 import ArtsView from './artsView/artsView.component';
 import LiteratureView from './literatureView/literatureView.component';
 import SearchView from './searchView/searchView.component';
 import TestView from './testView/testView.component';
 
+const GlobalView = (props) => {
 
-export default class GlobalView extends React.Component {
-
-  render () {
-    return (
-      <div className="globalView">
-        <div className="globalContainer">
+  return (
+    <div className="globalView">
+      <div className="globalContainer">
+        <Switch location={props.location} key={props.location.key}>
           <Route exact path='/' render={routeProps => (
             <HomeView
-              artistsData={this.props.artistsData}
-              authorsData={this.props.authorsData}
+              artistsData={props.artistsData}
+              authorsData={props.authorsData}
               {...routeProps} />
           )} />
           <Route path="/literature" render={routeProps => (
             <LiteratureView
-              authorsData={this.props.authorsData}
-              updateStore={this.props.updateStore}
-              updateCurrentAuthor={this.props.updateCurrentAuthor}
-              store={this.props.store}
+              authorsData={props.authorsData}
+              updateStore={props.updateStore}
+              updateCurrentAuthor={props.updateCurrentAuthor}
+              store={props.store}
               {...routeProps} />
           )} />
           <Route path="/arts" render={routeProps => (
             <ArtsView
-              artistsData={this.props.artistsData}
-              updateStore={this.props.updateStore}
-              store={this.props.store}
+              artistsData={props.artistsData}
+              updateStore={props.updateStore}
+              store={props.store}
               {...routeProps} />
           )} />
           <Route path="/search" render={routeProps => (
             <SearchView
-              artistsData={this.props.artistsData}
-              authorsData={this.props.authorsData}
-              searchInput={this.props.searchInput}
-              updateStore={this.props.updateStore}
-              store={this.props.store}
+              artistsData={props.artistsData}
+              authorsData={props.authorsData}
+              searchInput={props.searchInput}
+              updateStore={props.updateStore}
+              store={props.store}
               {...routeProps} />
           )} />
           <Route exact path="/test" component={TestView} />
-        </div>
+        </Switch>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default GlobalView;
