e904a7bcd42c732dd081ec198857b7f27264a2f6
Brendan Best <visarts@gmail.com>
2017-08-25 11:53:04 -0500

    add genre separation in authors view

--- src/app/globalView/literatureView/literatureAuthor/literatureAuthor.component.js
+++ src/app/globalView/literatureView/literatureAuthor/literatureAuthor.component.js
@@ -17,23 +17,48 @@ const LiteratureAuthor = (props) => {
   };
 
   const getTitles = (author) => {
-    return author.content.map((title, index) => {
+    //let genres = [[], [], [], [], []];
+    let genres = {
+      shorts: [],
+      poetry: [],
+      nonfiction: [],
+      fables: [],
+      tales: []
+    }
+    // 0: shorts
+    // 1: poetry
+    // 2: nonfiction
+    // 3: fables
+    // 4: tales
+    author.content.map((title, index) => {
       title.author = author;
-      return (
-        <ListLink
-          key={title.fileName}
-          url={`/literature/${author.creatorKey}/${title.fileName}`}
-          action={openTitle.bind(this, title)}
-          text={title.title} />
-      );
+      let titleLink = (<ListLink
+        key={title.fileName}
+        url={`/literature/${author.creatorKey}/${title.fileName}`}
+        action={openTitle.bind(this, title)}
+        text={title.title} />);
+      switch(title.genre) {
+        case 'shorts':
+          genres.shorts.push(titleLink);
+          break;
+        case 'poetry':
+          genres.poetry.push(titleLink);
+          break;
+        case 'nonfiction':
+          genres.nonfiction.push(titleLink);
+          break;
+        case 'fables':
+          genres.fables.push(titleLink);
+          break;
+        case 'tales':
+          genres.tales.push(titleLink);
+          break;
+      }
     });
+    return genres;
   };
 
   const titles = getTitles(author);
-
-
-
-
   const loadDefaultProfileImage = (event) => {
     event.target.src='./content/portraits/profile.jpg';
   };
@@ -48,9 +73,31 @@ const LiteratureAuthor = (props) => {
         <div className="bio">{author.bio}<div className="readMoreLink"><a href="#" target="_blank">Read More <Glyphicon glyph="new-window" /></a></div></div>
       </div>
       <div className="titlesContainer">
-        <div className="titlesTitle"><h3>Explore the library</h3></div>
-        <ul className="titles">{ titles }</ul>
-        <div className="titlesFadeOut"></div>
+        {titles.shorts[0] && <div className="genreContainer">
+          <div className="titlesTitle"><h3>Short Stories</h3></div>
+          <ul className="titles">{ titles.shorts }</ul>
+          <div className="titlesFadeOut"></div>
+        </div>}
+        {titles.poetry[0] && <div className="genreContainer">
+          <div className="titlesTitle"><h3>Poetry</h3></div>
+          <ul className="titles">{ titles.poetry }</ul>
+          <div className="titlesFadeOut"></div>
+        </div>}
+        {titles.nonfiction[0] && <div className="genreContainer">
+          <div className="titlesTitle"><h3>Nonfiction</h3></div>
+          <ul className="titles">{ titles.nonfiction }</ul>
+          <div className="titlesFadeOut"></div>
+        </div>}
+        {titles.fables[0] && <div className="genreContainer">
+          <div className="titlesTitle"><h3>fables</h3></div>
+          <ul className="titles">{ titles.fables }</ul>
+          <div className="titlesFadeOut"></div>
+        </div>}
+        {titles.tales[0] && <div className="genreContainer">
+          <div className="titlesTitle"><h3>Tales</h3></div>
+          <ul className="titles">{ titles.tales }</ul>
+          <div className="titlesFadeOut"></div>
+        </div>}
         <BackToTop />
       </div>
     </div>
