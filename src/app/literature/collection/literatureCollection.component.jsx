97b2f406bf0323e5f88fea13ed93ced29e6524e8
Brendan Best <visarts@gmail.com>
2018-01-02 20:20:27 -0600

    add colletion splitting for authors in genres

--- src/app/literature/collection/literatureCollection.component.jsx
+++ src/app/literature/collection/literatureCollection.component.jsx
@@ -5,18 +5,31 @@ const Collection = (props) => {
 
   const collection = props.globalStore.collections[props.globalState.routing.collection];
 
+  const creators = {};
+
+  collection.items.map((item, key) => {
+    const creatorId = item.id.split('-')[0];
+    const creator = props.globalStore.collections[creatorId];
+    creators[creatorId] = creators[creatorId] ? creators[creatorId] : [];
+
+    creators[creatorId].push(
+      <li key={key}>
+        <Link to={`/literature/${props.globalState.routing.collection}/${item.id}`}>
+          {item.name} by {creator.name.first} {creator.name.last}
+        </Link>
+      </li>
+    );
+  });
+
   return (
     <div className="literature_collection">
       <h1>{collection.name}</h1>
-      <ul>
-        {collection.items.map((item, key) => (
-          <li key={key}>
-            <Link to={`/literature/${props.globalState.routing.collection}/${item.id}`}>
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {creators && Object.keys(creators).map((item, key) => (
+        <div className="section" key={key}>
+          <h2>{props.globalStore.collections[item].name.last}</h2>
+          <ul>{creators[item]}</ul>
+        </div>
+      ))}
     </div>
   );
 };
--- src/app/literature/creator/literatureCreator.component.jsx
+++ src/app/literature/creator/literatureCreator.component.jsx
@@ -22,3 +22,41 @@ const Creator = (props) => {
 };
 
 export default Creator;
+
+import { Link } from 'react-router-dom';
+import './literatureCollection.style.scss';
+
+const Collection = (props) => {
+
+  const collection = props.globalStore.collections[props.globalState.routing.collection];
+
+  const creators = {};
+
+  collection.items.map((item, key) => {
+    const creatorId = item.id.split('-')[0];
+    const creator = props.globalStore.collections[creatorId];
+    creators[creatorId] = creators[creatorId] ? creators[creatorId] : [];
+
+    creators[creatorId].push(
+      <li key={key}>
+        <Link to={`/literature/${props.globalState.routing.collection}/${item.id}`}>
+          {item.name} by {creator.name.first} {creator.name.last}
+        </Link>
+      </li>
+    );
+  });
+
+  return (
+    <div className="literature_collection">
+      <h1>{collection.name}</h1>
+      {creators && Object.keys(creators).map((item, key) => (
+        <div className="section" key={key}>
+          <h2>{props.globalStore.collections[item].name.last}</h2>
+          <ul>{creators[item]}</ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Collection;
