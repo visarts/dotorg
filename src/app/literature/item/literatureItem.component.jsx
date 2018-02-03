955ba2415782717f2597f459bfa9271470836cef
Brendan Best <visarts@gmail.com>
2018-02-03 12:17:17 -0600

    add pagination functionality for lit modal, add and stylize material ui buttons, fix issue with andersen lit item

--- src/app/literature/item/literatureItem.component.jsx
+++ src/app/literature/item/literatureItem.component.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
+import Button from 'material-ui/Button';
 import Modal from 'material-ui/Modal';
 import './literatureItem.style.scss';
 
 const Item = (props) => {
 
   const item = props.globalStore.items.find(item => item.id === props.globalState.routing.item);
+  // create a portitude modal component that has all this functional stuff built in
   const hideModal = () => {
     location.hash = location.hash.substring(0, location.hash.lastIndexOf('/'));
   };
@@ -20,9 +22,13 @@ const Item = (props) => {
             <div>{props.globalStore.collections[item.category].name}</div>
           </div>
           <div className="portitudeModal--body">
-            <div className="modalContent" dangerouslySetInnerHTML={props.content} />
+            <div className="modalContent" dangerouslySetInnerHTML={{__html: props.content}} />
           </div>
-          <div className="portitudeModal--footer">Prev || Next</div>
+          <div className="portitudeModal--footer">
+            <Button className="pagination--button" color="primary" raised onClick={props.setPreviousPage} disabled={props.isFirstPage}>Previous</Button>
+            <span style={{padding: '10px'}}>{props.currentPage + 1}</span>
+            <Button className="pagination--button" color="primary" raised onClick={props.setNextPage} disabled={props.isLastPage}>Next</Button>
+          </div>
         </div>
       </Modal>
     </div>
--- src/app/literature/item/literatureItem.container.js
+++ src/app/literature/item/literatureItem.container.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import _ from 'lodash';
 import dataService from 'Services/data.service';
 import Item from './literatureItem.component';
 
@@ -6,24 +7,74 @@ export default class ItemContainer extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      content: false
+      content: false,
+      currentPage: 0
     };
+    this.item = this.props.globalStore.items.find(item => item.id === props.globalState.routing.item);
+    this.pages = [];
+    this.setPages = this.setPages.bind(this);
+    this.setFirstPage = this.setFirstPage.bind(this);
+    this.setLastPage = this.setLastPage.bind(this);
+    this.setNextPage = this.setNextPage.bind(this);
+    this.setPreviousPage = this.setPreviousPage.bind(this);
     this.setContent.call(this);
+  }
+
+  // take the HTML and split it into an array by the items precaculated page sizes
+  setPages (content) {
+    let remainingContent = content;
+    _.map(this.item.pageSizes, pageSize => {
+      this.pages.push(remainingContent.substring(0, pageSize));
+      remainingContent = remainingContent.substring(pageSize);
+    });
   }
 
   setContent () {
     const itemId = this.props.globalState.routing.item;
     const authorId = itemId.split('-')[0];
     dataService.getHTMLContent(authorId, itemId)
-      .then(results => {
-        this.setState({content: {__html: results}});
+      .then(content => {
+        this.setPages(content);
+        this.setState({content: this.pages[this.state.currentPage]});
       });
+  }
+
+  setFirstPage () {
+    const currentPage = 0;
+    this.setState({content: this.pages[currentPage], currentPage});
+  }
+
+  setLastPage () {
+    const currentPage = this.pages.length - 1;
+    this.setState({content: this.pages[currentPage], currentPage});
+  }
+
+  setNextPage () {
+    const currentPage = this.state.currentPage + 1;
+    console.log(this.pages);
+    this.setState({content: this.pages[currentPage], currentPage});
+  }
+
+  setPreviousPage () {
+    const currentPage = this.state.currentPage - 1;
+    this.setState({content: this.pages[currentPage], currentPage});
   }
 
   render () {
     return (
       <div>
-        {this.state.content && <Item {...this.props} content={this.state.content} />}
+        {this.state.content &&
+          <Item
+            {...this.props}
+            content={this.state.content}
+            setFirstPage={this.setFirstPage}
+            setLastPage={this.setLastPage}
+            setNextPage={this.setNextPage}
+            setPreviousPage={this.setPreviousPage}
+            isFirstPage={this.state.currentPage === 0}
+            isLastPage={this.state.currentPage === this.pages.length - 1}
+            currentPage={this.state.currentPage} />
+        }
       </div>
     );
   }
