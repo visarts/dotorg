b498717318868bd4281429ce49df00fb37739205
Brendan Best <visarts@gmail.com>
2018-02-23 16:11:31 -0600

    start making changes to data service for lit and art services

--- src/app/_services/data.service.js
+++ src/app/_services/data.service.js
@@ -4,7 +4,6 @@ import _ from 'lodash'
 import storeService from 'Services/store.service'
 
 const { artwork, literature } = storeService.getStore()
-console.log(literature)
 /*const getAuthorData = (author) => {
   const authorData = require(`authors/${author}.json`)
   return authorData
@@ -78,16 +77,17 @@ const getLiteratureCreator = creatorId => {
 }
 
 // gets all creators and their items in a given collection
-const getLiteratureCreatorsByCollection = (collectionId) => {
+const getLiteratureCreatorsByCollection = collectionId => {
   const collection = literature.collections[collectionId]
-  const parsedCollection = _.map(collection.items, item => {
+  const filteredItems = _.map(collection.items, item => {
     const creatorId = item.id.split('-')[0]
     const creator = getLiteratureCreator(creatorId)
+
   })
 }
 
 // gets all collections and their items with a given creator
-const getLiteratureCollectionsByCreator = (creatorId) => {
+const getLiteratureCollectionsByCreator = creatorId => {
 
 }
 
