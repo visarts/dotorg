2fb63de3bd6731fe99e9099d038f9227b9bba7f8
visarts <visarts@gmail.com>
2018-04-20 13:01:01 -0500

    start adding style switching to typography based on type, add babel object spread support

--- src/app/_common/typography/typography.style.js
+++ src/app/_common/typography/typography.style.js
@@ -1,10 +1,19 @@
 import styled, { css } from 'styled-components'
 
-export const StyledTypography = styled(({ element, children }) => (
-  React.createElement(element, null, children)
-))`
+export const StyledTypography = styled(({element, children, type, ...other}) => {
+  return React.createElement(element, {...other}, children)
+})`
   opacity: 1;
   transition: .2s all;
+  ${props => props.type === 'headline' && css`
+    font-size: calc(1rem + 1vw);
+  `}
+  ${props => props.type === 'headlineLarge' && css`
+    font-size: calc(1rem + 1vw + 10px);
+  `}
+  ${props => props.type ==='subheadline' && css`
+    font-size: calc(.8rem + 1vw);
+  `}
   ${props => props.type === 'listPrimary' && css`
     font-size: 1.2rem;
   `}
