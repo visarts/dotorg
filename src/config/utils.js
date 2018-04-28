60a1327e97cda074f77a4f71b83d31c664760f23
visarts <visarts@gmail.com>
2018-04-27 22:26:21 -0500

    fix issues with small media utils

--- src/config/utils.js
+++ src/config/utils.js
@@ -3,19 +3,18 @@ import { css } from 'styled-components'
 
 // input any css arguments for small media views
 export const smallScreen = (...args) => css`
-  @media (max-width: 768px) {
+  @media (max-width: ${themeConfig.breakpoints.md}) {
     ${css(...args)}
   }
 `
-
 export const globalGutters = () => {
   return `
     padding-left: ${themeConfig.spacing.globalGutter};
     padding-right: ${themeConfig.spacing.globalGutter};
-    ${smallScreen`
+    @media (max-width: ${themeConfig.breakpoints.md}) {
       padding-left: ${themeConfig.padding.lg};
       padding-right: ${themeConfig.padding.lg};
-    `}
+    }
   `
 }
 
