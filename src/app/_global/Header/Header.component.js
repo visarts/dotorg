24f3ec20e3a7f55ae0fdfc9a2cb252f93f698cfc
Brendan Best <visarts@gmail.com>
2018-05-05 23:53:12 -0500

    refactor header to remove list elements

--- src/app/_global/Header/Header.component.js
+++ src/app/_global/Header/Header.component.js
@@ -3,7 +3,7 @@ import Typography from 'common/typography'
 import Favorite from 'material-ui-icons/Favorite'
 import './globalHeader.style.scss'
 
-const HeaderComponent = props => {
+const GlobalHeaderComponent = props => {
   const { navigationData, globalState } = props
   const { routing } = globalState
   const levels = {
@@ -12,42 +12,34 @@ const HeaderComponent = props => {
   }
 
   return (
-    <div className="header">
-      <ul className="header-navigation">
-        <li>
-          <Link to={navigationData.root.fullPath} className="header-navigation-item">
-            <span className="header-navigation-icon"><Favorite /></span>
-            <Typography type={routing.section ? 'headline' : 'headlineLarge'} style={{opacity: levels.root}}>
-              {navigationData.root.name}
+    <div className="header" id="portitude-header">
+      <div className="header-navigation">
+        <Link to={navigationData.root.fullPath} className="header-navigation-item">
+          <span className="header-navigation-icon"><Favorite /></span>
+          <Typography type={routing.section ? 'headline' : 'headlineLarge'} style={{opacity: levels.root}}>
+            {navigationData.root.name}
+          </Typography>
+        </Link>
+        {navigationData.section.name &&
+          <Link to={navigationData.section.fullPath} className="header-navigation-item">
+            <Typography type="headline" style={{opacity: levels.section}}>
+              {navigationData.section.name}
             </Typography>
           </Link>
-        </li>
-        {navigationData.section.name &&
-          <li>
-            <Link to={navigationData.section.fullPath} className="header-navigation-item">
-              <Typography type="headline" style={{opacity: levels.section}}>
-                {navigationData.section.name}
-              </Typography>
-            </Link>
-          </li>
         }
         {navigationData.collection.name &&
-          <li>
-            <Link to={navigationData.collection.fullPath} className="header-navigation-item">
-              <Typography type="headline">{navigationData.collection.type === 'category' ? navigationData.collection.name : navigationData.collection.name.last}</Typography>
-            </Link>
-          </li>
+          <Link to={navigationData.collection.fullPath} className="header-navigation-item">
+            <Typography type="headline">{navigationData.collection.type === 'category' ? navigationData.collection.name : navigationData.collection.name.last}</Typography>
+          </Link>
         }
         {navigationData.item.name && false &&
-          <li>
-            <Link to={navigationData.item.fullPath} className="header-navigation-item">
-              <h1>{navigationData.item.name}</h1>
-            </Link>
-          </li>
+          <Link to={navigationData.item.fullPath} className="header-navigation-item">
+            <h1>{navigationData.item.name}</h1>
+          </Link>
         }
-      </ul>
+      </div>
     </div>
   )
 }
 
-export default HeaderComponent
+export default GlobalHeaderComponent
