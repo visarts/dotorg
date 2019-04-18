f449913dab2ebdd722efbc3341f187c11c9324fc
Brendan Best <visarts@gmail.com>
2019-04-17 20:30:45 -0500

    update eslint, final commit before refactor number 8 million

--- src/components/_common/Typography/typography.style.js
+++ src/components/_common/Typography/typography.style.js
@@ -5,29 +5,31 @@ export const StyledTypography = styled(({element, children, type, gutterBottom, 
 })`
   opacity: 1;
   transition: .2s all;
-  ${props => props.type === 'headline' && css`
+  ${props => css`
+    ${props.type === 'headline' && css`
     font-size: calc(1rem + 1vw);
     margin: 0px;
-  `}
-  ${props => props.type === 'headlineLarge' && css`
-    font-size: calc(1rem + 1vw + 10px);
-    margin: 0px;
-  `}
-  ${props => props.type ==='subheadline' && css`
-    font-size: calc(.8rem + 1vw);
-  `}
-  ${props => props.type === 'listPrimary' && css`
-    font-size: 1.2rem;
-  `}
-  ${props => props.type === 'listSecondary' && css`
-    font-size: .9rem;
-    color: ${props.theme.colors.foreground.dark2};
-  `}
-  ${props => props.type === 'paragraph' && css`
-    margin: 0;
-    margin-bottom: 1rem;
-  `}
-  ${props => props.gutterBottom && css`
-    margin-bottom: 15px;
+    `}
+    ${props.type === 'headlineLarge' && css`
+      font-size: calc(1rem + 1vw + 10px);
+      margin: 0px;
+    `}
+    ${props.type ==='subheadline' && css`
+      font-size: calc(.8rem + 1vw);
+    `}
+    ${props.type === 'listPrimary' && css`
+      font-size: 1.2rem;
+    `}
+    ${props.type === 'listSecondary' && css`
+      font-size: .9rem;
+      color: ${props.theme.colors.foreground.dark2};
+    `}
+    ${props.type === 'paragraph' && css`
+      margin: 0;
+      margin-bottom: 1rem;
+    `}
+    ${props.gutterBottom && css`
+      margin-bottom: 15px;
+    `}
   `}
 `
